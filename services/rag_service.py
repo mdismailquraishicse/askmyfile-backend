e6b81e2b530007aec26cb0ebec4859ea2bf3c06e
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2026-05-28 11:20:27 +0530

    feat: define LLMService and RAGService classes

---
+++ services/rag_service.py
@@ -0,0 +1,25 @@
+from services.llm_service import LLMService
+from services.retrieval_service import RetrievalService
+
+
+
+class RAGService:
+
+
+    def __init__(self):
+        
+        self.retrieval = RetrievalService()
+        self.llm = LLMService()
+
+
+    def ask(self, query:str, collection_name:str):
+
+        context = self.retrieval.get_context(query = query, collection_name = collection_name)
+        answer = self.llm.generate(
+            question = query,
+            context = context
+        )
+        return {
+            "answer": answer,
+            "context": context
+        }
