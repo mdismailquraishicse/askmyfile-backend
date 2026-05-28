60d4c91531bb18b1dcbf1141531357b4cb70b9a1
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2026-05-28 10:32:14 +0530

    refactor: separate codes

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import shutil
 import uvicorn
-from fastapi import FastAPI, WebSocket, File, UploadFile
-from starlette.websockets import WebSocketDisconnect
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from services.ask_my_file_services import AskMyFileService
-
+from api.routes.upload import router as upload_router
+from api.routes.chat import router as chat_router
 
 
 app = FastAPI()
-askmyfile_service = AskMyFileService()
-sockets : set[WebSocket] = set()
-collection_name = "my_collection"
+
+app.include_router(upload_router)
+
+app.include_router(chat_router)
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,36 +19,6 @@ app.add_middleware(
     allow_methods = ["*"],
     allow_headers = ["*"]
 )
-
-
-@app.post("/upload-file")
-def upload_file(file: UploadFile = File(...)):
-
-    print(f"filename: {file.filename}")
-    file_path = "uploads/file.filename"
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
-    askmyfile_service.delete_collection(collection_name = collection_name)
-    askmyfile_service.create_collection(collection_name = collection_name)
-    askmyfile_service.upsert_embeddings_to_vector_db(path = file_path, collection_name = collection_name)
-
-@app.websocket("/ask")
-async def ask(websocket: WebSocket):
-
-    try:
-        sockets.add(websocket)
-        await websocket.accept()
-        while True:
-            query = await websocket.receive_text()
-            print(f"text recieved: {query}")
-            # await websocket.send_json({"answer": "answer", "context": "context"})
-            response = askmyfile_service.invoke_llm(query = query)
-            print(f"response: {response}")
-            await websocket.send_json(response)
-    except WebSocketDisconnect as wsd:
-        print(f"Websocket disconnected : {wsd}")
-        sockets.remove(websocket)
 
 
 if __name__ == "__main__":
