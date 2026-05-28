5a5fa0a8646ec49608137b5fe0a081f642e7e4b2
Md Ismail Quraishi <mdismailquraishicse@gmail.com>
2026-05-28 11:00:30 +0530

    feat: define class RetrievalService

---
+++ services/retrieval_service.py
@@ -0,0 +1,30 @@
+import os
+from db.qdrant_db import VectorDB
+from services.embedding_service import EmbeddingService
+
+
+
+
+class RetrievalService:
+
+
+    def __init__(self):
+        
+        self.embedding_service = EmbeddingService()
+        self.db = VectorDB(host=os.getenv("HOST", "localhost"),
+                           port=os.getenv("PORT", 6333))
+
+
+    def get_context(self, query:str, collection_name:str, k:int):
+
+        embedding = self.embedding_service.embed_query(query = query)
+        fetched_data = self.db.search_similar_docs(
+            embedding = embedding,
+            collection_name = collection_name,
+            k = k
+        )
+
+        context = "\n\n".join(
+            [data.payload.get("text") for data in fetched_data.points]
+        )
+        return context
